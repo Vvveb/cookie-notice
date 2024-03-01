d948ffed7ddb25656635fcfe32234a43986785b9
givanz <givanz@yahoo.com>
2024-03-01 21:26:05 +0200

    Settings url fix

--- app/template/common.tpl
+++ app/template/common.tpl
@@ -5,5 +5,5 @@ body|append = from(/public/plugins/cookie-notice/notice.html|body > #cookie-noti
 body|append = from(/public/plugins/cookie-notice/notice.html|body > #cookie-notice-js)
 
 //don't show the cookie notice if page is edited
-#cookie-notice|hide    = $is_editor
-#cookie-notice-js|hide = $is_editor
+#cookie-notice|hide    = $vvveb_is_page_edit
+#cookie-notice-js|hide = $vvveb_is_page_edit
--- plugin.php
+++ plugin.php
@@ -3,22 +3,22 @@
 /**
  * Vvveb
  *
- * Copyright (C) 2022  Ziadin Givan
+ * Copyright (C) 2023  Ziadin Givan
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
+ * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- */
+*/
 
 /*
 Name: Cookie Notice
@@ -30,7 +30,7 @@ Author: givanz
 Version: 0.1
 Thumb: cookie-notice.svg
 Author url: https://www.vvveb.com
-Settings: /admin/?module=editor/editor&url=/plugins/cookie-notice/notice.html&template=/plugins/cookie-notice/notice.html
+Settings: /admin/index.php?module=editor/editor&url=/plugins/cookie-notice/notice.html&template=/plugins/cookie-notice/notice.html
 */
 
 use function Vvveb\__;
@@ -47,7 +47,7 @@ class CookieNoticePlugin {
 		//add admin menu item
 		$admin_path = \Vvveb\adminPath();
 		$publicPath = \Vvveb\publicUrlPath();
-		$url        = url(['module' => 'editor/editor', 'url' => $publicPath . 'plugins/cookie-notice/notice.html', 'template' => '/plugins/cookie-notice/notice.html']);
+		$url        = url(['module' => 'editor/editor', 'url' => $publicPath . '/plugins/cookie-notice/notice.html', 'template' => '/plugins/cookie-notice/notice.html']);
 
 		Event::on('Vvveb\Controller\Base', 'init-menu', __CLASS__, function ($menu) use ($admin_path, $url) {
 			$menu['plugins']['items']['cookie-notice'] = [
